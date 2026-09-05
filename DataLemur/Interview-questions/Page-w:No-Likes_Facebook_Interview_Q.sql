4c39fbe96d36c5e3f504de85135652bc4868eb57
Samira Kazimi <skazimi@usc.edu>
2026-09-04 23:26:38 -0700

    Add Facebook interview question about pages w/no likes

---
+++ DataLemur/Interview-questions/Page-w:No-Likes_Facebook_Interview_Q.sql
@@ -0,0 +1,16 @@
+-- Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
+-- Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs.
+
+-- Table: pages
+-- Columns: page_id: int, page_name: varchar
+
+-- Table: page_likes
+-- Columns: user_id: int, page_id: int, liked_date: datetime
+
+
+SELECT pages.page_id
+FROM pages
+LEFT JOIN page_likes
+  ON pages.page_id = page_likes.page_id
+WHERE page_likes.page_id IS NULL
+ORDER BY page_id;
