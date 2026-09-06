429b1a7db6947e0bb817855299bd9a266b0ff44a
Samira Kazimi <skazimi@usc.edu>
2026-09-05 23:53:30 -0700

    Add a SQL full outer join practice question

---
+++ DataLemur/intermediate-SQL/SQL-FULL-OUTER-JOIN-Practice.sql
@@ -0,0 +1,10 @@
+-- Picture this: you want to find out how many books priced at $20 and above have been ordered, and who the buyers are. Suppose we want to retrieve all the orders along with their corresponding deliveries information.
+
+SELECT 
+  orders.order_id, 
+  deliveries.delivery_id, 
+  deliveries.delivery_date, 
+  deliveries.delivery_status
+FROM orders
+FULL OUTER JOIN deliveries
+  ON orders.order_id = deliveries.order_id;
